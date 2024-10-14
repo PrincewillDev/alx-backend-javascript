d52862db19f662bbed18d022df07b6211e538aa3
PrincewillDev <princewillfidelis1@gmail.com>
2024-10-14 17:23:20 +0100

    create holberton class

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,50 @@
+export default class HolbertonCourse {
+  /**
+   * Constructor for HolbertonCourse
+   * @param {string} name - Name of the course
+   * @param {number} length - Length of the course
+   * @param {string[]} students - Array of student names
+   */
+  constructor(name, length, students = []) {
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new Error('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw new Error('Students must be an array');
+    }
+    if (!students.every((students) => typeof students === 'string')) {
+      throw new Error('All students must be strings');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    this._length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    this._students = students;
+  }
+}
