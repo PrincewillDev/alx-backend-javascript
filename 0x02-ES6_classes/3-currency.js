4f421f48ffb06ff9f507530fc8750d06c235e990
PrincewillDev <princewillfidelis1@gmail.com>
2024-10-15 16:51:04 +0100

    Fixed format error

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,39 +1,39 @@
-export default class Currency{
-    /**
+export default class Currency {
+  /**
    * Constructor for Currency
    * @param {string} code - Currency code
    * @param {string} name - Name of the currency
    */
   constructor(code, name) {
-    if (typeof code !== 'string') {
-        throw new TypeError('Code must be a string');
-      }
-    if (typeof name !== 'string') {
-        throw new TypeError('Name must be a string');
+    if (typeof code !== "string") {
+      throw new TypeError("Code must be a string");
+    }
+    if (typeof name !== "string") {
+      throw new TypeError("Name must be a string");
     }
 
     this._code = code;
     this._name = name;
   }
 
-  get code(){
+  get code() {
     return this._code;
   }
 
-  set code(code){
-    if (code !== 'string'){
-        throw new TypeError('Code must be a string');
+  set code(code) {
+    if (code !== "string") {
+      throw new TypeError("Code must be a string");
     }
     this._code = code;
   }
 
-  get name(){
+  get name() {
     return this._name;
   }
 
-  set name(name){
-    if (typeof name !== 'string') {
-        throw new TypeError('Name must be a string');
+  set name(name) {
+    if (typeof name !== "string") {
+      throw new TypeError("Name must be a string");
     }
     this._name = name;
   }
