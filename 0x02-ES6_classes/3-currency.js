ccb1a60e005b3137d6a774ff614c7984339e337e
PrincewillDev <princewillfidelis1@gmail.com>
2024-10-15 16:12:56 +0100

    Created Currency Object

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -5,12 +5,12 @@ export default class Currency{
    * @param {string} name - Name of the currency
    */
   constructor(code, name) {
-    // if (typeof code !== 'string') {
-    //     throw new TypeError('Code must be a string');
-    //   }
-    // if (typeof name !== 'string') {
-    //     throw new TypeError('Name must be a string');
-    // }
+    if (typeof code !== 'string') {
+        throw new TypeError('Code must be a string');
+      }
+    if (typeof name !== 'string') {
+        throw new TypeError('Name must be a string');
+    }
 
     this._code = code;
     this._name = name;
@@ -21,9 +21,9 @@ export default class Currency{
   }
 
   set code(code){
-    // if (code !== 'string'){
-    //     throw new TypeError('Code must be a string');
-    // }
+    if (code !== 'string'){
+        throw new TypeError('Code must be a string');
+    }
     this._code = code;
   }
 
@@ -32,9 +32,9 @@ export default class Currency{
   }
 
   set name(name){
-    // if (typeof name !== 'string') {
-    //     throw new TypeError('Name must be a string');
-    // }
+    if (typeof name !== 'string') {
+        throw new TypeError('Name must be a string');
+    }
     this._name = name;
   }
 
