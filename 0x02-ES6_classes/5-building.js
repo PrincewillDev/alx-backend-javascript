9f90b65f4c192e9d13601fcfbae154d9dded5c16
PrincewillDev <princewillfidelis1@gmail.com>
2024-10-16 16:53:32 +0100

    Fixed formatting issue

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,22 @@
-export default class Building{
-    constructor(sqft) {
-        // if (typeof sqft !== 'number') {
-        //     throw new TypeError('sqft must be a number');
-        // }
-        
-        this._sqft = sqft;
+export default class Building {
+  constructor(sqft) {
+    // if (typeof sqft !== 'number') {
+    //     throw new TypeError('sqft must be a number');
+    // }
 
-        if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
-            throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
-    }
+    this._sqft = sqft;
 
-    get sqft() {
-        return this._sqft;
+    if (
+      this.constructor !== Building &&
+      this.evacuationWarningMessage === undefined
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage'
+      );
     }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
 }
