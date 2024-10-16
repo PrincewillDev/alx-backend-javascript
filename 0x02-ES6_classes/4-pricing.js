d8f4fec56c1260a3b966bd5d5b7be8907c747e6f
PrincewillDev <princewillfidelis1@gmail.com>
2024-10-16 16:15:08 +0100

    Created a class for Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,49 @@
+import Currency from "./3-currency";
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== "number") {
+      throw new TypeError("amount must be a number");
+    }
+
+    if (!(currency instanceof Currency)) {
+      throw new TypeError("currency must be an instance of Currency");
+    }
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== "number") {
+      throw new TypeError("amount must be a number");
+    }
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError("currency must be an instance of Currency");
+    }
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== "number" || typeof conversionRate !== "number") {
+      throw new TypeError("Both amount and conversionRate must be numbers");
+    }
+    return amount * conversionRate;
+  }
+}
