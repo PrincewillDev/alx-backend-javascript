6e682cbfd28fd903c100297ef9c26b4e223d5ef7
PrincewillDev <princewillfidelis1@gmail.com>
2024-10-16 17:09:56 +0100

    Create a class SkyHighBuilding that extends from Building

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,20 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft); // Call the parent class constructor to assign sqft
+    if (typeof floors !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
+    this._floors = floors; // floors is specific to the subclass
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    console.log(`Evacuate slowly the ${this._floors} floors`);
+  }
+}
