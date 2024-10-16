7dedf5bb9401328941d36bd94bb1bd86ff73e0ef
PrincewillDev <princewillfidelis1@gmail.com>
2024-10-16 21:37:51 +0100

    Corrected syntax error and added getter and setter

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -15,8 +15,23 @@ export default class Airport {
     this._code = code;
   }
 
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
   // Overriding toString to return the airport code
   toString() {
-    return this._code;
+    return `[object ${this._code}]`;
   }
 }
