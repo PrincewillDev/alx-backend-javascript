55bc975f75e3356afb9a7eab27e275d84848e792
PrincewillDev <princewillfidelis1@gmail.com>
2024-10-16 21:56:14 +0100

    Corrected syntax error and added getter and setter

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,16 +1,15 @@
 export default class Airport {
+
   constructor(name, code) {
-    // Type checking for name
     if (typeof name !== 'string') {
       throw new TypeError('name must be a string');
     }
 
-    // Type checking for code
     if (typeof code !== 'string') {
       throw new TypeError('code must be a string');
     }
 
-    // Assigning to underscore attributes
+
     this._name = name;
     this._code = code;
   }
@@ -36,7 +35,7 @@ export default class Airport {
     }
     this._code = code;
   }
-  // Overriding toString to return the airport code
+
   toString() {
     return `[object ${this._code}]`;
   }
