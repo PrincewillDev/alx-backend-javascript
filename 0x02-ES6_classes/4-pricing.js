f7b26326ebc185ed44a3a415e613102d12e7cd2c
PrincewillDev <princewillfidelis1@gmail.com>
2024-10-16 16:20:47 +0100

    Change double quote texts to single quotes

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,13 +1,13 @@
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    if (typeof amount !== "number") {
-      throw new TypeError("amount must be a number");
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
     }
 
     if (!(currency instanceof Currency)) {
-      throw new TypeError("currency must be an instance of Currency");
+      throw new TypeError('currency must be an instance of Currency');
     }
 
     this._amount = amount;
@@ -19,8 +19,8 @@ export default class Pricing {
   }
 
   set amount(amount) {
-    if (typeof amount !== "number") {
-      throw new TypeError("amount must be a number");
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
     }
     this._amount = amount;
   }
@@ -31,7 +31,7 @@ export default class Pricing {
 
   set currency(currency) {
     if (!(currency instanceof Currency)) {
-      throw new TypeError("currency must be an instance of Currency");
+      throw new TypeError('currency must be an instance of Currency');
     }
     this._currency = currency;
   }
@@ -41,8 +41,8 @@ export default class Pricing {
   }
 
   static convertPrice(amount, conversionRate) {
-    if (typeof amount !== "number" || typeof conversionRate !== "number") {
-      throw new TypeError("Both amount and conversionRate must be numbers");
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Both amount and conversionRate must be numbers');
     }
     return amount * conversionRate;
   }
