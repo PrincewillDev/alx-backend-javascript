8fe662c71a3161dee2ef3fe1cc724046a41c3b4a
PrincewillDev <princewillfidelis1@gmail.com>
2024-10-16 16:56:25 +0100

    Fixed formatting issue

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,8 @@
 export default class Building {
   constructor(sqft) {
-    // if (typeof sqft !== 'number') {
-    //     throw new TypeError('sqft must be a number');
-    // }
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
 
     this._sqft = sqft;
 
