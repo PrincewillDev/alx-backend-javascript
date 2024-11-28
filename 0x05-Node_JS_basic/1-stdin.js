8459834f18c29f7f75fe05f4e484d523c1e29e4c
PrincewillDev <princewillfidelis1@gmail.com>
2024-11-28 10:22:11 +0100

    This program take input from stdin and display result

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,26 @@
 // This program take input from stdin and display result
-const readline = require('readline');
+// const readline = require('readline');
 
-const getInput = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+// const getInput = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// getInput.question(
+//   'Welcome to Holberton School, what is your name?\n',
+//   (input) => {
+//     console.log(`Your name is: ${input}`);
+//   },
+// );
+
+// process.on('exit', () => console.log('This important software is now closing'));
+
+const message = 'Welcome to Holberton School, what is your name?\n';
+
+process.stdout.write(message);
+
+process.stdin.on('data', (input) => console.log(`Your name is: ${input}`));
+
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
 });
-
-getInput.question(
-  'Welcome to Holberton School, what is your name?\n',
-  (input) => {
-    console.log(`Your name is: ${input}`);
-  },
-);
-
-process.on('exit', () => console.log('This important software is now closing'));
